cfc1142cde979cb0181c12bdbdaad342385c06f4
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-02-05 23:49:05 +0200

    Functions Advanced Exercise

--- functions_advanced/exercise_07_grocery.py
+++ functions_advanced/exercise_07_grocery.py
@@ -5,21 +5,3 @@ def grocery_store(**kwargs):
     for item in sorted_list:
         result.append(f"{item[0]}: {item[1]}")
     return "\n".join(result)
-
-
-print(grocery_store(
-    bread=5,
-    pasta=12,
-    eggs=12,
-))
-
-
-print(grocery_store(
-    bread=2,
-    pasta=2,
-    eggs=20,
-    carrot=1,
-    apple=2,
-    corns=2,
-    dogss=2
-))
--- functions_advanced/exercise_08_age_assigment.py
+++ functions_advanced/exercise_08_age_assigment.py
@@ -11,9 +11,3 @@ def age_assignment(*names, **letter_and_age):
     person_information = [f"{name} is {age} years old." for name, age in data_tracking]
 
     return "\n".join(person_information)
-
-
-
-
-print(age_assignment("Peter", "George", G=26, P=19))
-print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
